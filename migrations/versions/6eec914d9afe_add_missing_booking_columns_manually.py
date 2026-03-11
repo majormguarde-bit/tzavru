e6f1663436453ab5ef4b96f69c7874b69e09291d
guard-e <majorm@yandex.ru>
2026-03-11 22:44:20 +0300

    Fix all conflicting migrations by disabling redundant operations

--- migrations/versions/6eec914d9afe_add_missing_booking_columns_manually.py
+++ migrations/versions/6eec914d9afe_add_missing_booking_columns_manually.py
@@ -17,12 +17,8 @@ depends_on = None
 
 
 def upgrade():
-    with op.batch_alter_table('booking', schema=None) as batch_op:
-        batch_op.add_column(sa.Column('confirmation_code', sa.String(length=10), nullable=True))
-        batch_op.add_column(sa.Column('is_email_confirmed', sa.Boolean(), nullable=True))
+    pass
 
 
 def downgrade():
-    with op.batch_alter_table('booking', schema=None) as batch_op:
-        batch_op.drop_column('is_email_confirmed')
-        batch_op.drop_column('confirmation_code')
+    pass
